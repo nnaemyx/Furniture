2b32cdf78a215453e521e082e52b8ff5dab49c33
tezon <tezonteam@gmail.com>
2022-09-14 16:57:51 -0700

    new

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+// import components
+import TestimonialSlider from './TestimonialSlider';
+
+// import data
+import { testimonial } from '../data';
 
 const Testimonial = () => {
+  const {image, title} = testimonial;
   return (
-    <div>Testimonial</div>
+    <section className="section">
+      <div className="container max-auto">
+        <div className='flex flex-col lg:flex-row lg:gap-x-10 gap-y-2'>
+          <div className='lg:max-w-[50%]'>
+            {/* title */}
+            <h2 className='title mb-9'>{title}</h2>
+            {/* slider */}
+            <TestimonialSlider/>
+          </div>
+          {/* image */}
+          <div className="order-1">
+            <img src={image.type} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/TestimonialSlider.jsx
+++ src/components/TestimonialSlider.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+// import swiper react components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// import swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+// import required modules
+import { Autoplay, Navigation } from 'swiper';
+import { fromJSON } from 'postcss';
+
+// import data
+import {testimonial } from '../data'
 const TestimonialSlider = () => {
-  return <div>TestimonialSlider</div>;
-};
+  return (
+    <Swiper>
+      {testimonial.persons.map((person, index) => {
+        // destructuring
+        const { avatar, name, occupation, message } = person;
+        return (
+          <SwiperSlide key={index}>
+            <div>
+              <div>
+                {/* avatar */}
+                <img src={avatar.type} alt="" />
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
+  )
+}
 
-export default TestimonialSlider;
+export default TestimonialSlider
