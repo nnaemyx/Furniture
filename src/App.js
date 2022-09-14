87568386073a225c09c072f3c6ea0f94beb12101
tezon <tezonteam@gmail.com>
2022-09-14 09:49:17 -0700

    new

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import React from "react";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import NewItems from "./components/NewItems";
+import FeaturesSecond from './components/FeaturesSecond';
+import Products from "./components/Products";
 
 const App = () => {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
       <Hero />
       <Features />
-      <NewItems/>
+      <NewItems />
+      <FeaturesSecond />
+      <Products/>
     </div>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+// import data
+import { products } from '../data';
+
+// import components
+import ProductSlider from '../components/ProductSlider';
+
 const Products = () => {
-  return <div>Products</div>;
+  // destructuring
+  const { title, subtitle } = products;
+  return (
+    <section className='section'>
+      <div className='container mx-auto'>
+        <div>
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+        </div>
+        {/* slider */}
+        <ProductSlider/>
+      </div>
+    </section>
+  );
 };
 
 export default Products;
